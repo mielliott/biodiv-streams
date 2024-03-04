12af96a51d9492ffbcee3f9b25610bb59477dd91
Michael Elliott <mielliott@ufl.edu>
2024-03-04 17:50:51 +0000

    Remove shuffling

--- stream-idigbio.py
+++ stream-idigbio.py
@@ -7,18 +7,7 @@ RETRY_DELAY_IN_SECONDS = 2
 
 def make_query(page, page_size):
     return {
-        "rq": {
-            "uuid": {
-                "type": "range",
-                "gt": "00000000-0000-0000-0000-000000000000",
-                "lte": "ffffffff-ffff-ffff-ffff-ffffffffffff"
-                }
-            },
-        "sort": [
-                {
-                    "uuid": "asc"
-                }
-            ],
+        "rq": {},
         "limit": page_size,
         "offset": page
     }
